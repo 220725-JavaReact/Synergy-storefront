d3774b95d9528cf73dd32d16e5a61467d45ca344
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-07-25 14:25:07 -0700

    more info on item

--- src/com/revature/models/Item.java
+++ src/com/revature/models/Item.java
@@ -2,21 +2,60 @@ package com.revature.models;
 
 public class Item {
 	private final String name;
-	private final double price;
+	private double price;
 	private static int amount = 50;
 	private final String description;
 	private final String expiration;
 	
 	
-	public Item() {
-		super();
-	}
+	//not empty parameter constructor
 
-	public Item(String name, double price) {
+
+	public Item(String name, double price, String description, String expiration) {
 		super();
 		this.name = name;
 		this.price = price;
+		this.description = description;
+		this.expiration = expiration;
 	}
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public static int getAmount() {
+		return amount;
+	}
+
+
+	public static void setAmount(int amount) {
+		Item.amount = amount;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getDescription() {
+		return description;
+	}
+
+
+	public String getExpiration() {
+		return expiration;
+	}
+	
+	
+
 	
 	
 }
